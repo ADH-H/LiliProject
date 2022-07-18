ac85a0480f811dc575d97241d286bba1fba068c0
ADH-H <70413953+ADH-H@users.noreply.github.com>
2022-07-18 18:49:32 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from managers import lili_json_manager
 input_setting = input("Setting:\n ")
 input_value = input("Value:\n")
-lili_json_manager.liliConfig(setting=input_setting, value=input_value)
+lili_json_manager.currentlili(setting=input_setting, value=input_value)
